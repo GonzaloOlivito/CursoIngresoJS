ff76295dd5d0124b1f34f314dd166ebbc3933675
alumno <alumno>
2019-02-06 20:24:13 -0300

    Ejercicio 6 terminado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,27 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var primerNumero;
+var segundoNumero;
 
+
+primerNumero=numeroUno.value;
+segundoNumero=numeroDos.value;
+
+//parseInt(primerNumero);
+//parseInt(segundoNumero);
+
+
+primerNumero=parseInt(primerNumero);
+segundoNumero=parseInt(segundoNumero);
+
+suma=primerNumero+segundoNumero;
+
+
+//suma = (primerNumero+segundoNumero);
+alert("La suma es " +suma);
+
+
+//alert ("la suma es" +suma );
 }
 
