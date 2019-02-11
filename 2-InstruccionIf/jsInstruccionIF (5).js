31b9bf7f33514d88565ac2e9ffa90bc5591cce97
alumno <alumno>
2019-02-11 20:34:24 -0300

    Ejercicio IF 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+
+laEdad=edad.value;
+laEdad=parseInt(laEdad);
+
+if (laEdad<13 || laEdad>17)	
+{	
+	alert("No es adolescente")
+}
 
 
 }//FIN DE LA FUNCIÓN
