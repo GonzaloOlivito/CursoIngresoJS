10b44d4328beeaee5ad24745b4d9ae73f8442c47
alumno <alumno>
2019-02-11 19:32:34 -0300

    Ejercicio tp

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+var edadNiña;
 
+edadNiña=edad.value
 
+if(edadNiña ==15)
+//Si es verdadero
+// Si o si entre llaves, se van a ejecutar un conjunto de instrucciones
+{
+alert ("Niña bonita")
+}
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,62 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var suma;
 	
+	primerPrecio=PrecioUno.value;
+	segundoPrecio=PrecioDos.value;
+	tercerPrecio=PrecioTres.value;
+
+	precioEntero1=parseInt(primerPrecio);
+	precioEntero2=parseInt(segundoPrecio);
+	precioEntero3=parseInt(tercerPrecio);
+
+	suma=(precioEntero1 + precioEntero2 + precioEntero3);
+
+	alert("El total es " + suma);
+
 }
 function Promedio () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var promedio;
 	
+	primerPrecio=PrecioUno.value;
+	segundoPrecio=PrecioDos.value;
+	tercerPrecio=PrecioTres.value;
+
+	precioEntero1=parseInt(primerPrecio);
+	precioEntero2=parseInt(segundoPrecio);
+	precioEntero3=parseInt(tercerPrecio);
+
+	promedio=(precioEntero1 + precioEntero2 + precioEntero3) / 3;
+
+	alert("El promedio es " + promedio);
+
 }
 function PrecioFinal () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var precioParcial;
+	var PrecioFinal;
 	
+	primerPrecio=PrecioUno.value;
+	segundoPrecio=PrecioDos.value;
+	tercerPrecio=PrecioTres.value;
+
+	precioEntero1=parseInt(primerPrecio);
+	precioEntero2=parseInt(segundoPrecio);
+	precioEntero3=parseInt(tercerPrecio);
+
+	precioParcial=(precioEntero1 + precioEntero2 + precioEntero3);
+    PrecioFinal=(precioParcial*0.21) + precioParcial;
+	alert("El precio final es " + PrecioFinal);
+
 }
