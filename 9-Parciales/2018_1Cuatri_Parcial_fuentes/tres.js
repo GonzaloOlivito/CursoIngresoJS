69328dcd5be93846fe75e3e5518deccef0b186e3
alumno <alumno>
2019-02-11 21:17:41 -0300

    Ejercicio parcial 3 a medias

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+var precio;
+var porcentaje;
+var precioFinal;
+
+precio=prompt("Ingrese el precio");
+porcentaje=prompt("Ingrese el porcentaje a descontar");
+precio=parseInt(precio);
+porcentaje=parseInt(porcentaje);
+
+precioFinal=(precio*porcentaje);
+precioFinal=parseInt(precioFinal);
+
+elPrecioFinal.value=precioFinal;
+
+
 
 }
