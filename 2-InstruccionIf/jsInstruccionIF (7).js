fa9f2336860e636a24ed84fe56d41de69c012fe1
alumno <alumno>
2019-02-11 22:14:29 -0300

    Ejercicio 7 if incompleto

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+var laEdad;
+var elEstadocivil
+
+laEdad=edad.value;
+laEdad=parseInt(laEdad);
+
+elEstadocivil=estadoCivil.value;
+
+if(laEdad>18 && elEstadocivil=Soltero)
+{
+	alert ("asda");
+}
+
+
 
 
 }//FIN DE LA FUNCIÓN
