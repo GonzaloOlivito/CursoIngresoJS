1f122cd5cebde33e54b9321fb29998bced1f6dc2
unknown <gonzalo.olivito97@gmail.com>
2019-02-13 17:18:15 -0300

    ejercicio 1 recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+var perimetro;
+var ancho;
+var largo;
 
+largo=prompt("Ingrese el largo");
+ancho=prompt("Ingrese el ancho");
+
+largo=parseInt(largo);
+ancho=parseInt(ancho);
+
+perimetro=(largo + ancho) * 2;
+alert("El perimetro del rectangulo es " +perimetro);
 }
