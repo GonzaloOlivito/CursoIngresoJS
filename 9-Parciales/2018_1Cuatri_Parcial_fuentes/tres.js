2bee0aafe7b5bccd235e43393657748f8ae40bf5
unknown <gonzalo.olivito97@gmail.com>
2019-02-13 17:07:43 -0300

    ejercicio 3 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -4,15 +4,17 @@ var precio;
 var porcentaje;
 var precioFinal;
 var precioDescuento;
+var descuento;
 
 precio=prompt("Ingrese el precio");
 porcentaje=prompt("Ingrese el porcentaje a descontar");
+
 precio=parseInt(precio);
 porcentaje=parseInt(porcentaje);
 
-precioFinal=(precio*porcentaje) /100;
-precioFinal=parseInt(precioFinal);
-precioDescuento=(precioFinal+precio);
+descuento=(precio*porcentaje) /100;
+precioFinal=parseInt(descuento);
+precioDescuento=(precio-descuento);
 precioDescuento=parseInt(precioDescuento);
 
 elPrecioFinal.value=precioDescuento;
