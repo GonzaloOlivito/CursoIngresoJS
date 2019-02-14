1f28eaae872e0e997060f6a53ea1f838a48f4fbe
alumno <alumno>
2019-02-13 21:47:31 -0300

    ejercicio 10 if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+	var calificacion;
+	nota=Math.floor(Math.random() * 11);  
 
+	if(nota>4)
+	{
+		califacion="Aprobó";
+	if (nota <5)
+	califacion="Mas suerte la proxima";
+
+	}else
+	{
+		califacion="Excelente";
+	}
+
+	alert(califacion);
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+var elEstadocivil
 
-	
+laEdad=edad.value;
+laEdad=parseInt(laEdad);
+
+elEstadocivil=estadoCivil.value;
+
+if(laEdad<18 && elEstadocivil!="Soltero")
+{
+	alert ("Es muy pequeño para no ser soltero");
+}
+
+
 
 
 }//FIN DE LA FUNCIÓN
