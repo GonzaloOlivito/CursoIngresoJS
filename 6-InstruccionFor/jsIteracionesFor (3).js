6c90eec1ad707df49e36953b7dac8ffbc17f9d41
alumno <alumno>
2019-02-25 21:23:44 -0300

    clase 25/02

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,27 @@
 function mostrar()
 {
+var numeroIngresado;
+var numeroAnterior;
+var acumulador=0;
+numeroIngresado=prompt("ingrese un numero");
+numeroIngresado=parseInt(numeroIngresado);
 
-var repetciones = prompt("ingrese el número de repeticiones");
+for(recorrido=numeroIngresado-1;recorrido>1;recorrido--)
+{
 
 
+	for(numeroAnterior=recorrido-1;numeroAnterior>0;numeroAnterior--)
+		{
 
-}//FIN DE LA FUNCIÓN
+			if(numeroIngresado%numeroAnterior==0)
+				{
+				acumulador= acumulador+numeroAnterior;
+				}
+		}
+
+			if(acumulador==numeroIngresado)
+		{
+			console.log("es un numero perfecto" + numeroIngresado);
+		} 
+}		
+}
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+var nota;
+var sexo;
+var contador;
+var acumulador=0;
+var menor=10;
+var sexoMenor;
+	for(contador=0;contador<5;contador++)
+		{
+			nota=prompt("Ingrese la nota");
+			nota=parseInt(nota);
+						
+		while(nota<0 || nota>10)
+		{
+			nota=prompt("nota incorrecta, reintente");	
+			sexo=prompt("ingrese el sexo del alumno")
+		}
+		while(sexo!="m" && sexo!="f")
+		{
+			sexo=prompt("sexo invalido");
+		}
+		acumulador=acumulador+nota;
+			if(nota < menor)
+			{
+				menor=nota;
+				sexoMenor=sexo;
+			}
+		if(nota>5 && sexo == "m")
+			{
+				varonesAprobado= varonesAprobado+1;
+			}
 
+		acumulador=acumulador/5;
+
+		alert("El promedio de las notas es " +acumulador+ ", la nota mas baja es " +menor+ "es de sexo " +sexoMenor+ ", y la cantidad de varones aprobados es " +varonesAprobado);
+	}
 }
